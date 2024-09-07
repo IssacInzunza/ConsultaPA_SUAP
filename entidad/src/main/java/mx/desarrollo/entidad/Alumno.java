1291a5164e5a6a35cc1f76706281b52ab47c30b3
Antonio <antonio_brosgamer@hotmail.com>
2024-09-07 16:40:00 -0700

    primer cambio
    esta en entidad alumno.java

--- entidad/src/main/java/mx/desarrollo/entidad/Alumno.java
+++ entidad/src/main/java/mx/desarrollo/entidad/Alumno.java
@@ -20,7 +20,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
+//hola
 /**
  *
  * @author Johan
