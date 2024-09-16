456a6b44e2d62e2edbf99783fa9ab3bd70453cf1
IssacInzunza <151285011+IssacInzunza@users.noreply.github.com>
2024-09-16 06:57:36 -0700

    Se agrego un index para administrador

---
+++ mavenproject1/vista/src/main/java/mx/desarrollo/ui/beanUI/AdminBeanUI.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.desarrollo.ui.beanUI;
+
+import java.io.Serializable;
+import javax.faces.application.NavigationHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author issac
+ */
+@ManagedBean
+@ViewScoped
+public class AdminBeanUI implements Serializable{
+    
+    public void redirigirAConsultaProfesor() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            NavigationHandler navigationHandler = facesContext.getApplication().getNavigationHandler();
+            navigationHandler.handleNavigation(facesContext, null, "ConsultasProfesores.xhtml?faces-redirect=true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void redirigirAAltaProfesor() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            NavigationHandler navigationHandler = facesContext.getApplication().getNavigationHandler();
+            navigationHandler.handleNavigation(facesContext, null, "altaProfesores.xhtml?faces-redirect=true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void redirigirAConsultaUnidades() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            NavigationHandler navigationHandler = facesContext.getApplication().getNavigationHandler();
+            navigationHandler.handleNavigation(facesContext, null, "consultaUnidades.xhtml?faces-redirect=true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
